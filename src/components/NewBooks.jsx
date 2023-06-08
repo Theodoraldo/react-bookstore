2d67a328b254f9e8238bdcded084aaa7b158c775
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-08 17:18:08 +0000

    Modify Neewbooks.jsx file

--- src/components/NewBooks.jsx
+++ src/components/NewBooks.jsx
@@ -4,14 +4,7 @@ export default function NewBooks() {
       <h2>ADD NEW BOOK</h2>
       <form>
         <input type="text" placeholder="Book Title" />
-        <select name="">
-          <option value="Category" selected>
-            Category
-          </option>
-          <option value="Category A">Category A</option>
-          <option value="Category B">Category B</option>
-          <option value="Category C">Category C</option>
-        </select>
+        <input type="text" placeholder="Book Author" />
         <button>ADD BOOK</button>
       </form>
     </div>
