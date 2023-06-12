03b6d5e3c93375a98f28aa79769303abd934e8fb
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-12 15:52:25 +0000

    Configure store.js file

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './booksSlice';
+import categoriesReducer from './categoriesSlice';
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
 
 export default store;
