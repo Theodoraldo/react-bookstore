e6c0e793bfc4f28e009a13366a582b48a126c7e3
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-13 13:34:31 +0000

    Add remove functionality

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,38 +1,32 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import './styles/BookItem.css';
 
 const BookItem = () => {
   const books = useSelector((state) => state.books);
 
+  const dispatch = useDispatch();
+
+  const removeOldBook = (itemId) => {
+    dispatch(removeBook(itemId));
+  };
+
   return (
     <div>
       {books.map((book) => (
-        <div key={book.item_id} className="book-card">
-          <div>
-            <div>
-              <p>Title</p>
+        <div key={book.item_id}>
+          <div className="card-card">
+            <div className="book-card">
               <h3>{book.title}</h3>
               <p>Author</p>
               <p>{book.author}</p>
             </div>
             <div>
-              <button type="button">Comment</button>
-              <button type="button">Remove</button>
-              <button type="button">Edit</button>
+              <button type="button" onClick={() => removeOldBook(book.item_id)}>
+                Remove
+              </button>
             </div>
-          </div>
-          <div>
-            <p>Progress</p>
-            <div>
-              <div>50%</div>
-              <div>Completed</div>
-            </div>
-          </div>
-          <div>
-            <p>Current Chapter</p>
-            <p>Chapter</p>
-            <button type="button">UPDATE PROGRESS</button>
           </div>
         </div>
       ))}
