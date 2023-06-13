3c81ed12380505a067d905b0ccb4fbce0b26563b
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-13 08:53:54 +0000

    Modify components

--- src/components/BookLogic.jsx
+++ src/components/BookLogic.jsx
@@ -1,37 +1,10 @@
-import BookList from './BookList';
 import BookInput from './BookInput';
+import BookItem from './BookItem';
 
 export default function Books() {
-  const books = [
-    {
-      id: 1,
-      type: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      percentage: 64,
-      chapter: 'Chapter 17',
-    },
-    {
-      id: 2,
-      type: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-      percentage: 8,
-      chapter: 'Chapter 3: A Lesson Learned',
-    },
-    {
-      id: 3,
-      type: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-      percentage: 0,
-      chapter: 'Introduction',
-    },
-  ];
-
   return (
     <>
-      <BookList bookProps={books} />
+      <BookItem />
       <hr />
       <BookInput />
     </>
