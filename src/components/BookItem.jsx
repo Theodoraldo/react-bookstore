736961fd8bb64e2e9826e633f7cd467370be81e8
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-13 11:05:33 +0000

    Add display of books from redux store

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,34 +1,43 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './styles/BookItem.css';
 
-const BookItem = () => (
-  <>
-    <div className="item-container">
-      <div>
-        <div>
-          <p>type</p>
-          <h3>title</h3>
-          <p>author</p>
+const BookItem = () => {
+  const books = useSelector((state) => state.books);
+
+  return (
+    <div>
+      {books.map((book) => (
+        <div key={book.item_id} className="book-card">
+          <div>
+            <div>
+              <p>Title</p>
+              <h3>{book.title}</h3>
+              <p>Author</p>
+              <p>{book.author}</p>
+            </div>
+            <div>
+              <button type="button">Comment</button>
+              <button type="button">Remove</button>
+              <button type="button">Edit</button>
+            </div>
+          </div>
+          <div>
+            <p>Progress</p>
+            <div>
+              <div>50%</div>
+              <div>Completed</div>
+            </div>
+          </div>
+          <div>
+            <p>Current Chapter</p>
+            <p>Chapter</p>
+            <button type="button">UPDATE PROGRESS</button>
+          </div>
         </div>
-        <div>
-          <button type="button">Comment</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </div>
-      </div>
-      <div>
-        <p>Progress</p>
-        <div>
-          <div>percentage</div>
-          <div>Completed</div>
-        </div>
-      </div>
-      <div>
-        <p>Current Chapter</p>
-        <p>chapter</p>
-        <button type="button">UPDATE PROGRESS</button>
-      </div>
+      ))}
     </div>
-  </>
-);
+  );
+};
 
 export default BookItem;
