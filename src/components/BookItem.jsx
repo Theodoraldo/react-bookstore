5956eee7b8e142c976c43269dca9731b3c332d76
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-15 09:22:57 +0000

    Modify files to render books

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,31 +1,30 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { useEffect } from 'react';
+import { getBooks, removeBook } from '../redux/books/booksSlice';
 import './styles/BookItem.css';
 
 const BookItem = () => {
-  const books = useSelector((state) => state.books);
-
   const dispatch = useDispatch();
-
+  const books = useSelector((state) => state.books.books);
+  console.log(books);
   const removeOldBook = (itemId) => {
     dispatch(removeBook(itemId));
   };
 
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+
   return (
     <div>
-      {books.map((book) => (
-        <div key={book.item_id} className="section">
-          <div className="card-card">
-            <h3>{book.title}</h3>
-            <p>{book.author}</p>
-            <button
-              className="remove-btn"
-              type="button"
-              onClick={() => removeOldBook(book.item_id)}
-            >
-              Remove
-            </button>
-          </div>
+      {Object.entries(books).map(([itemId, book]) => (
+        <div key={itemId} className="book-card">
+          <h3>{book[0].title}</h3>
+          <p>{book[0].author}</p>
+          <p>{book[0].category}</p>
+          <button type="button" onClick={() => removeOldBook(book.item_id)}>
+            Remove
+          </button>
         </div>
       ))}
     </div>
