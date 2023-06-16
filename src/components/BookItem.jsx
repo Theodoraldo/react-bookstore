0e484da40daf6f9ac8e97dbb827625d191d81d70
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-16 15:55:25 +0000

    Add styling to books display

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -13,17 +13,21 @@ const BookItem = () => {
 
   return (
     <div>
-      {isLoading && <div>Loading ........</div>}
-      {error && <div>Ooops! Something happend whiles fetching data</div>}
+      {isLoading && <div className="alert alert-info">Loading ........</div>}
+      {error && (
+        <div className="alert alert-danger">
+          Ooops! Something happend whiles fetching data
+        </div>
+      )}
       {!isLoading &&
         Object.entries(books).map(([itemId, book]) => (
           <div className="card-card">
             <div>
               <div>
-                <p>{book[0].category}</p>
-                <h4>{book[0].title}</h4>
-                <p>{book[0].author}</p>
-                <div>
+                <p className="trans">{book[0].category}</p>
+                <h4 className="trans">{book[0].title}</h4>
+                <p className="trans">{book[0].author}</p>
+                <div className="trans">
                   <button type="button" className="btn">
                     Comment
                   </button>
@@ -41,16 +45,17 @@ const BookItem = () => {
               </div>
             </div>
             <div>
-              <p>50%</p>
+              <p className="trans">50%</p>
               <div>
-                <div>{book.percentage}</div>
-                <div>Completed</div>
+                <div className="trans">Completed</div>
               </div>
             </div>
-            <div>
-              <p>Current Chapter</p>
+            <div className="trans">
+              <p className="trans">Current Chapter</p>
               <p>{book.chapter}</p>
-              <button type="button">UPDATE PROGRESS</button>
+              <button className="btn" type="button">
+                UPDATE PROGRESS
+              </button>
             </div>
           </div>
         ))}
